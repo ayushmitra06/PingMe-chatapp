d0b4e8ebf44c870613e2ae6319eaec92997cc75a
Ayush <ayushmitra06@gmail.com>
2025-02-02 21:54:54 +0530

    Project completed

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+
+// Zustand store for theme
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("chat-theme") || "coffee",
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    set({ theme });
+  },
+}));
